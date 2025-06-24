e48aa00a48d25227c977798e386c19643553860d
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-06-24 12:13:56 +0000

    debug

--- aws/modules/aurora/versions.tf
+++ aws/modules/aurora/versions.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.35"
+    rhcs = {
+      version = "~> 1.4"
+      source  = "terraform-redhat/rhcs"
     }
   }
 }
